fa575f6c52642932adea7413b7321f2811e56050
Ivan Trendafilov <ivan.trendafiloff@gmail.com>
2018-11-01 21:32:09 +0200

    Added exception handling for TypeError and ValueError

--- exceptional.py
+++ exceptional.py
@@ -4,5 +4,10 @@ A module for demonstrationg exceptions.
 
 def convert(s):
     '''Convert to an integer.'''
-    x = int(s)
+    x = -1
+    try:
+        x = int(s)
+        print('Conversion succeeded! x =', x)
+    except (ValueError, TypeError):
+        print("Conversion failed!")
     return x
