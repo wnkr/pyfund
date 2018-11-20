c5d41a607d2ea0da9007dc30ff12c73cf87e0e2b
Ivan Trendafilov <ivan.trendafiloff@gmail.com>
2018-11-20 17:24:25 +0200

    Now use 'with blocks'

--- demos/series.py
+++ demos/series.py
@@ -3,11 +3,8 @@
 import sys
 
 def read_series(filename):
-    try:
-        f = open(filename, mode='rt', encoding='utf-8')
+    with open(filename, mode='rt', encoding='utf-8') as f:
         return [ int(line.strip()) for line in f ]
-    finally:
-        f.close()
 
 def main(filename):
     series = read_series(filename)
