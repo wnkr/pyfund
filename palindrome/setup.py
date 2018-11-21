dec3f2ba6fa132c078069df8d7ea221dc5cae753
Ivan Trendafilov <ivan.trendafiloff@gmail.com>
2018-11-21 13:17:34 +0200

    Added setup module for the palindrome module.

---
+++ palindrome/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='palindrome',
+    version='1.0',
+    py_modules=['palindrone'],
+
+    # metadata
+    author='Ivan Trendafilov',
+    author_email='ivan.trendafiloff@gmail.com',
+    description='A module for finding palindromic numbers.',
+    license='Public domain',
+    keywords='example',
+)
